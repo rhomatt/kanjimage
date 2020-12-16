d6468a1ec53055e1f4d9cccabbbd18f9f355ddbb
rhomatt <rhomatt@gmail.com>
2020-12-15 21:30:28 -0500

    can paste images into the browser now

--- src/component/Imagebox.js
+++ src/component/Imagebox.js
@@ -1,20 +1,41 @@
 import {useState, useEffect} from 'react';
 
 function Imagebox(){
-    const [imageExists, setImageExists] = useState(false);
+    const [image, setImage] = useState(null);
 
     const pasteHandler = (event) => {
+            const items = event.clipboardData.items;
+
+            var blob = null;
+            for(var i = 0; i < items.length; i++){ // get the first image
+                let item = items[i];
+                console.log(item);
+                if(item.type === 'image/png'){
+                    blob = item.getAsFile();
+                    break;
+                }
+            }
+
+        var reader = new FileReader();
+        reader.onload = (event) => {
+            let result = event.target.result
+            console.log(result);
+            setImage(result);
+        };
+
+        if(blob) // should be null if not an image
+            reader.readAsDataURL(blob);
 
     };
 
     useEffect(() => {
         document.addEventListener('paste', pasteHandler);
-    });
+    }, []);
 
     return(
         <>
-            {imageExists ? undefined : 'no image'}
-            <img/>
+        {image ? undefined : 'no image'}
+        <img src={image}/>
         </>
     );
 }
